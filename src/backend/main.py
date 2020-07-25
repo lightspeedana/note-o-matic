17ce468e69469d04451aa0a5b8e1d302c49e09f9
kali <aputravu1234@gmail.com>
2020-07-25 15:30:57 -0400

    word2vec

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 import nltk
+import gensim.models
+
+import numpy as np
 
 import utils.fetch as fetch
 import utils.text as text
@@ -9,7 +12,9 @@ URL = "https://en.wikipedia.org/wiki/United_States"
 title, paragraphs = fetch.parse_webpage(URL)
 stopwords = nltk.corpus.stopwords.words('english')
 cleaned = text.clean_text(paragraphs, stopwords)
+# text.generate_wordcloud(' '.join(cleaned), stopwords)
 
-print(cleaned)
+model1 = gensim.models.Word2Vec(cleaned, min_count=1, size=100, window=5)
+wvTing = model1.wv
 
-text.generate_wordcloud(' '.join(cleaned), stopwords)
+
