bee2b2daba900a3a47a3d3737a396ab923fecd0a
kali <aputravu1234@gmail.com>
2020-07-25 18:41:07 -0400

    word2vec

--- src/backend/main.py
+++ src/backend/main.py
@@ -21,24 +21,8 @@ title, paragraphs = fetch.parse_webpage(URL)
 stopwords = nltk.corpus.stopwords.words('english')
 cleaned = text.clean_text(paragraphs, stopwords)
 unique = set(cleaned)
+
 # text.generate_wordcloud(' '.join(cleaned), stopwords)
 
-model = gensim.models.Word2Vec([unique], min_count=1, size=len(unique), window=5)
-
-neighbors = NearestNeighbors(n_neighbors=2)
-nbrs = neighbors.fit(model.wv.vectors)
-t = time.time()
-distances, _ = nbrs.kneighbors(model.wv.vectors)
-distances = np.sort(distances[:,1], axis=0)
-epsilon = np.average(distances[:len(distances)//2])
-
-t = time.time()
-clustering = DBSCAN(eps=epsilon, min_samples=5).fit(model.wv.vectors)
-print(f"Time taken to DBSCAN : {time.time() - t}")
-
-n_clusters_ = len(set(clustering.labels_)) - (1 if -1 in clustering.labels_ else 0)
-n_noise_ = list(clustering.labels_).count(-1)
-
-print(f"Clusters: {n_clusters_} | Noise: {n_noise_}")
-
+models.word2vec_model([unique], verbose=True, min_count=1, size=len(unique), window=5)
 
--- src/backend/utils/models.py
+++ src/backend/utils/models.py
@@ -1,8 +1,25 @@
 import gensim
 import numpy as np
+import sklearn.cluster
+import sklearn.neighbors
+
 
 from typing import List
 
-def word2vec_model(unique_text: List[str], *args):
+def word2vec_model(unique_text: List[str], verbose: bool=False, *args):
     model = gensim.models.Word2Vec([unique_text], *args)
+    vec = model.wv.vectors
+    
+    # Get optimal epslion
+    nbrs = sklearn.neighbors.fit(vec)
+    distances, _ = nbrs.kneighbors(vec)
+    distances = np.sort(distances[:,1], axis=0)
+    epsilon = np.average(distances[:len(distances)//2])
+    
+    db = sklearn.cluster.DBSCAN(eps=epsilon, min_samples=5).fit(vec)
+    n_clusters = len(set(db.labels_)) - (1 if -1 in db.labels_ else 0)
+    n_noise = list(db.labels_).count(-1)
+    if verbose:
+        print(f"Clusters: {n_clusters_} | Noise: {n_noise_}")
+
 
