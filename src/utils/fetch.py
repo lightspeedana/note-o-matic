d3ac74b5f2dbb395c9053d47bda669fe2577ec52
rak1507 <56049432+rak1507@users.noreply.github.com>
2020-07-25 16:33:40 +0100

    pep 8 go brrrr

--- src/utils/fetch.py
+++ src/utils/fetch.py
@@ -1,6 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-
 from typing import Union
 
 def parse_webpage(url: str) -> Union[str, str]:
@@ -13,7 +12,7 @@ def parse_webpage(url: str) -> Union[str, str]:
     """
     r = requests.get(url)
     if r.status_code != 200:
-	    return None
+        return None
     soup = BeautifulSoup(r.content, 'html.parser')
     paragraphs = [x.get_text() for x in soup.find_all('p')]
     title = soup.find('h1').get_text()
