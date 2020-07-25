bf66a0dab71dc6247c7067af9df55d369b0e2294
sam <tritoke@protonmail.com>
2020-07-25 18:46:52 +0100

    Removed tabs and reformatted code with black.

--- src/backend/utils/fetch.py
+++ src/backend/utils/fetch.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from typing import Union
 
+
 def parse_webpage(url: str) -> Union[str, str]:
     """
         Get the content of a webpage or article to be able to look through
@@ -13,7 +14,8 @@ def parse_webpage(url: str) -> Union[str, str]:
     r = requests.get(url)
     if r.status_code != 200:
         return None
-    soup = BeautifulSoup(r.content, 'html.parser')
-    paragraphs = (x.get_text() for x in soup.find_all('p'))
-    title = soup.find('h1').get_text()
-    return title, ' '.join(paragraphs)
+
+    soup = BeautifulSoup(r.content, "html.parser")
+    paragraphs = (x.get_text() for x in soup.find_all("p"))
+    title = soup.find("h1").get_text()
+    return title, " ".join(paragraphs)
--- src/backend/utils/text.py
+++ src/backend/utils/text.py
@@ -11,44 +11,60 @@ from math import log
 
 
 def clean_text(text: str, stopwords: List[str]) -> List[str]:
-    text = re.sub(r"[\"\(\)]", ' ', text).lower()
-    text = re.sub(r"[\-\_]", '', text) 
-    lem  = WordNetLemmatizer()
+    text = re.sub(r"[\"\(\)]", " ", text).lower()
+    text = re.sub(r"[\-\_]", "", text)
+    lem = WordNetLemmatizer()
     if not isinstance(stopwords, set):
         stopwords = set(stopwords)
-    return [lem.lemmatize(w) for w in word_tokenize(text) if (w not in stopwords and not re.match(r"^.*\W.*$", w))]
+    return [
+        lem.lemmatize(w)
+        for w in word_tokenize(text)
+        if (w not in stopwords and not re.match(r"^.*\W.*$", w))
+    ]
 
-def calculate_tfidf(text: List[str], ndocs: int=1) -> Dict[str, float]:
+
+def calculate_tfidf(text: List[str], ndocs: int = 1) -> Dict[str, float]:
     count = Counter(text)
     length = len(text)
     # Calculate tf
-    tfidf = {k : v / length for k, v in count.items()}
-    # Calculate idf 
+    tfidf = {k: v / length for k, v in count.items()}
+    # Calculate idf
     for k, v in tfidf.items():
-	    idf = log(ndocs / count[k])
-	    tfidf[k] *= idf
+        idf = log(ndocs / count[k])
+        tfidf[k] *= idf
     return tfidf
 
+
 def generate_wordcloud(text: str, stopwords: List[str]) -> None:
-    def random_color_func(word=None, font_size=None, position=None,  orientation=None, font_path=None, random_state=None):
+    def random_color_func(
+        word=None,
+        font_size=None,
+        position=None,
+        orientation=None,
+        font_path=None,
+        random_state=None,
+    ):
         h = int(360.0 * 21.0 / 255.0)
         s = int(100.0 * 255.0 / 255.0)
         l = int(100.0 * float(random_state.randint(60, 120)) / 255.0)
 
         return "hsl({}, {}%, {}%)".format(h, s, l)
 
-    wordcloud = WordCloud(background_color='white',
-                            stopwords=stopwords,
-                            max_words=100,
-                            width=800, 
-                            height=600,
-			    color_func=random_color_func,
-                            max_font_size=150).generate(str(text))
-    fig = plt.figure(1, figsize=(20,10))
+    wordcloud = WordCloud(
+        background_color="white",
+        stopwords=stopwords,
+        max_words=100,
+        width=800,
+        height=600,
+        color_func=random_color_func,
+        max_font_size=150,
+    ).generate(str(text))
+    fig = plt.figure(1, figsize=(20, 10))
     plt.imshow(wordcloud)
-    plt.axis('off')
+    plt.axis("off")
     # plt.show()
-    plt.savefig('wordcloud.png', bbox_inches='tight')
+    plt.savefig("wordcloud.png", bbox_inches="tight")
+
 
 def most_common_ngrams(text, n):
     ngrams = list(nltk.ngrams(text, n))
