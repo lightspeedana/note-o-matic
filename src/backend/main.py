7a10a6ceee299ec55867850cab92967ab3366456
Kali <rory.kemp04@gmail.com>
2020-07-25 19:56:26 -0400

    clustering yay

--- src/backend/main.py
+++ src/backend/main.py
@@ -20,13 +20,15 @@ URL = "https://en.wikipedia.org/wiki/Gerald_B._Greenberg"
 title, paragraphs = fetch.parse_webpage(URL)
 stopwords = nltk.corpus.stopwords.words('english')
 cleaned = text.clean_text(paragraphs, stopwords)
-unique = set(cleaned)
+unique = list(set(cleaned)) # cast to list so stuff is in the same place, just in case stuff does a fucky wucky otherwise
 
 # text.generate_wordcloud(' '.join(cleaned), stopwords)
 
-model, db = models.word2vec_model(unique, min_count=1, window=5, verbose=True)
+model, db, clusters, n_clusters, n_noise = models.word2vec_model(unique, min_count=1, window=5, verbose=True)
 
 print('-'*40)
-print(db.labels_.shape, len(unique))
+print(clusters.shape, len(unique))
 
+word_clusters = [np.array(unique)[clusters == i] for i in range(n_clusters)]
 
+print(*word_clusters, sep='\n')
--- src/backend/utils/models.py
+++ src/backend/utils/models.py
@@ -3,14 +3,16 @@ import numpy as np
 import sklearn.cluster
 import sklearn.neighbors
 
-
 from typing import List
 
-def word2vec_model(unique_text: set, min_count: int=1, window: int=5, n_neighbors: int=2, min_samples: int=5, verbose: bool=False) -> None: 
+def word2vec_model(unique_text: set, min_count: int=1, window: int=5, n_neighbors: int=2, min_samples: int=4, verbose: bool=False):
+    """
+    Returns a word2vec model, a dbscan, clusters, the number of clusters, and the amount of noise
+    """
     model = gensim.models.Word2Vec([unique_text], min_count=min_count, size=len(unique_text), window=window)
     vec = model.wv.vectors
     
-    # Get optimal epslion
+    # Get optimal epsilon
     neighbors = sklearn.neighbors.NearestNeighbors(n_neighbors=n_neighbors)
     nbrs = neighbors.fit(vec)
     distances, _ = nbrs.kneighbors(vec)
@@ -19,10 +21,10 @@ def word2vec_model(unique_text: set, min_count: int=1, window: int=5, n_neighbor
     
     # Run cluster detection ting
     db = sklearn.cluster.DBSCAN(eps=epsilon, min_samples=min_samples).fit(vec)
-    n_clusters = len(set(db.labels_)) - (1 if -1 in db.labels_ else 0)
-    n_noise = list(db.labels_).count(-1)
+    clusters = db.labels_
+    n_clusters = len(set(clusters)) - (-1 in clusters)
+    n_noise = list(clusters).count(-1)
     if verbose:
         print(f"Clusters: {n_clusters} | Noise: {n_noise}")
     
-    return model, db
-
+    return model, db, clusters, n_clusters, n_noise
