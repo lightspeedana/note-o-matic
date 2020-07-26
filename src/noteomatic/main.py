160b8f68f317105e7b46e308c5d630eaa04d40c2
kali <aputravu1234@gmail.com>
2020-07-26 10:01:36 -0400

    flask app update

--- src/noteomatic/main.py
+++ src/noteomatic/main.py
@@ -10,11 +10,9 @@ import io
 import logging
 import nltk
 
-import noteomatic.backend.utils.models as models
-import noteomatic.backend.utils.text as text
+import backend.utils.models as models
+import backend.utils.text as text
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
 
 app = Flask(__name__)
 STOPWORDS = nltk.corpus.stopwords.words('english')
@@ -39,21 +37,6 @@ def make_notes():
     image = text.generate_wordcloud(' '.join(cleaned), STOPWORDS)
     return render_template("results.html", percentage=percentage, notes=notes.split('\n'), image=image, clusters=clusters, n_clusters=n_clusters, n_noise=n_noise, word_clusters=word_clusters)
 
-
-@app.route('/plot.png')
-def plot_png():
-    fig = create_figure()
-    output = io.BytesIO()
-    FigureCanvas(fig).print_png(output)
-    return Response(output.getvalue(), mimetype='image/png')
-
-def create_figure():
-    fig = Figure()
-    axis = fig.add_subplot(1, 1, 1)
-    xs = range(100)
-    ys = [random.randint(1, 50) for x in xs]
-    axis.plot(xs, ys)
-    return fig
 
 @app.errorhandler(500)
 def server_error(e):
